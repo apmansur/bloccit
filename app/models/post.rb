316d0b1382d119e769d043c074ceb3d2d066583d
Andrea Mansur <apmansur1993@gmail.com>
2017-06-05 01:54:49 +0000

    show errors for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,10 @@
 class Post < ActiveRecord::Base
     belongs_to :topic
-    validates :body, :title, uniqueness: true
+    
     has_many :comments, dependent: :destroy
+    
+   validates :title, length: { minimum: 5 }, presence: true
+   validates :body, length: { minimum: 20 }, presence: true
+   validates :topic, presence: true
+   validates :body, :title, uniqueness: true
 end
